913de391dfdddb4748e9f3769842ff4fdbf94367
Alan Ribeiro <78491224+Pnda0@users.noreply.github.com>
2022-09-06 00:53:20 -0300

    Update Print_tela.py

--- Print_tela.py
+++ Print_tela.py
@@ -1,6 +1,7 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 import time
+from selenium.webdriver.common.by import By
 
 
 def getscreenshotofurl(url):
@@ -8,11 +9,11 @@ def getscreenshotofurl(url):
     chrome_options.add_argument('--headless')
     chrome_options.add_argument('--start-maximized')
 
-    driver = webdriver.Chrome('C:\Python\chromedriver.exe', options=chrome_options)
+    driver = webdriver.Chrome('E:\Python\Python\chromedriver.exe', options=chrome_options)
     driver.get(url)
     time.sleep(2)
 
-    element = driver.find_element_by_xpath('//*[@id="cnt"]')
+    element = driver.find_element(By.XPATH,'/html/body/ytd-app/div[1]/ytd-page-manager/ytd-browse/ytd-two-column-browse-results-renderer/div[1]/ytd-rich-grid-renderer')
     width = 1920
     height = element.size['height'] + 1000
     driver.set_window_size(width, height)
@@ -20,7 +21,6 @@ def getscreenshotofurl(url):
     driver.save_screenshot('Printeste.png')
     driver.quit()
 
-getscreenshotofurl('https://www.google.com/search?q=chrome+driver&oq=chrome&aqs=chrome.0.69i59j69i57j69i60l3j5l3.596j0j7&sourceid=chrome&ie=UTF-8')
-
+getscreenshotofurl('https://www.youtube.com/')
 
 
