e39a24215d3d2c418cbb0547ac07aae7dd5d844f
Pnda0 <alan_carvalho2015@hotmail.com>
2022-09-29 07:59:12 -0300

    testes

---
+++ teste.py
@@ -0,0 +1,74 @@
+# Media de Idades
+
+#idades = [21, 12, 15, 22, 66, 34]
+#media = 0 
+
+#for i in range (6):
+    #media = media + idades[i]
+ 
+#print(media / 6)
+
+
+# Vetor sequencial
+
+#lista = [111,222,333,444,555,666,777,888,999]
+
+#busca = 444
+
+#i = 0
+
+#for i in lista:
+#    i == i + 1
+#    print(i)
+#    if i == busca:
+#        print('Chegamos onde queriamos')
+
+# Busca Sequencial
+
+#lista = [111,222,333,444,555,666,777,888,999]
+
+#esquerda = lista[0]
+#direita = lista[-1]
+
+
+#meio = direita + esquerda
+#metade = meio / 2
+#print(metade)
+
+#for esquerda in lista:
+#    esquerda == esquerda + 1
+#    print(esquerda)
+#    if esquerda == metade:
+#        print('Esquerda chegou primeiro')
+#        break
+
+
+#for direita in lista[::-1]:
+#    direita == direita + 1
+#    print(direita)
+#    if direita == metade:
+#        print('Direita chegou primeiro')
+#        break
+
+# Função
+
+#def my_function(linha):
+#    print('Escrever linha' + linha)
+
+#my_function('111')
+#my_function('222')
+#my_function('333')
+
+#print(my_function)
+
+# Função 02
+
+#def my_function(*kids):
+#    print('essa gurizada é jovem ne?' + kids[0])
+
+#my_function('joaquim', 'bill', 'lantejola')
+
+#print(my_function)
+
+
+
