8e1494e51249285a1c677f5c94567f46b87bae42
devadpromotora <alan_carvalho2015@hotmail.com>
2023-03-03 10:56:54 -0300

    importante

--- 365/extrai_link.py
+++
@@ -1,5 +0,0 @@
-
-# extrai o link da mensagem
-link_inicio = mensagem_texto.find('https://www.bet365.com/')
-link_fim = mensagem_texto.find('\n', link_inicio)
-link = mensagem_texto[link_inicio:link_fim]
